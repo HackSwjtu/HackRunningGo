0b125dcd817315ed9765dd3322e2932d6bad5125
Desgard_Duan <desgard_duan@foxmail.com>
2016-10-25 08:23:04 +0800

    create tp.data func

--- updateRoutes.py
+++ updateRoutes.py
@@ -3,6 +3,9 @@ import requests
 import json
 import re
 import math
+import time
+import datetime
+import random
 
 class Point:
     lat = 0
@@ -15,6 +18,9 @@ class Point:
     def __str__(self):
         return "x: " + str(self.lat) + ", y: " + str(self.lng)
 
+    # def __eq__(self, other):
+    #     return (self.lat == other.lat & self.lng == other.lng)
+
     def radian(self, data):
         return data * math.pi / 180.0
 
@@ -26,6 +32,7 @@ class Point:
         s = 2 * math.asin(math.sqrt(math.pow(math.sin(a / 2), 2) + math.cos(r1) * math.cos(r2) * math.pow(math.sin(b / 2), 2)))
         s = s * 6378.137
         return s * 1000
+
 
 
 headers = {
@@ -44,24 +51,29 @@ headers = {
         "Accept-Encoding": "gzip"
     }
 
+outdata = open('tp.data', 'w')
+
 testPoints = [
-        Point(103.985729,30.771025),
-        Point(103.987364,30.768690),
-        Point(103.988141,30.772452),
-        Point(103.989982,30.768566),
-        Point(103.990072,30.765339),
-        Point(103.990113,30.775152),
-        Point(103.991393,30.769404),
-        Point(103.991707,30.763314),
-        Point(103.992839,30.767362),
-        Point(103.993903,30.772670),
-        Point(103.995718,30.773321),
-        Point(103.996032,30.766797),
-        Point(103.996949,30.770715),
-        Point(103.998134,30.768291),
-        Point(103.998840,30.766111),
-        Point(104.000061,30.774981),
+        Point(30.771025, 103.985729),
+        Point(30.768690, 103.987364),
+        Point(30.772452, 103.988141),
+        Point(30.768566, 103.989982),
+        Point(30.765339, 103.990072),
+        Point(30.775152, 103.990113),
+        Point(30.769404, 103.991393),
+        Point(30.763314, 103.991707),
+        Point(30.767362, 103.992839),
+        Point(30.772670, 103.993903),
+        Point(30.773321, 103.995718),
+        Point(30.766797, 103.996032),
+        Point(30.770715, 103.996949),
+        Point(30.768291, 103.998134),
+        Point(30.766111, 103.998840),
+        Point(30.774981, 104.000061),
     ]
+
+def datetime_to_timestamp_in_milliseconds(d):
+    return int(time.mktime(d.timetuple()) * 1000)
 
 def getOriginalJson(roomId):
     url = 'http://gxapp.iydsj.com/api/v3/get/'+ str(roomId) + '/history/finished/record '
@@ -70,18 +82,40 @@ def getOriginalJson(roomId):
     return Request.content
 
 MAX_DISTANCE = 20
-selectedTestPoints = []
 
 def isSelectedPoint(lat, lng):
-    # print lat, lng
     thisPoint = Point(float(lat), float(lng))
     for tp in testPoints:
         dis = tp.dis(thisPoint)
-
         if dis <= MAX_DISTANCE:
-            selectedTestPoints.append(tp)
-            return 1
-    return 0
+            return 1, tp
+    return 0, 0
+
+
+def createFivePointsStr(points):
+    tpData = "{\"fivePointJson\":\"["
+    flag = datetime_to_timestamp_in_milliseconds(datetime.datetime.now())
+    id = int(random.uniform(0, 250))
+    cnt = 0
+
+    for point in points:
+        assert isinstance(point, Point)
+        need = "{\\\"flag\\\":%s,\\\"lon\\\":\\\"%s\\\",\\\"lat\\\":\\\"%s\\\",\\\"isFixed\\\":0,\\\"isPass\\\":false,\\\"isFinal\\\":false,\\\"id\\\":%s}" % (str(flag), str(point.lng), str(point.lat), str(id))
+        tpData += need
+        # print need
+        id += 1
+        cnt += 1
+        if cnt >= 4 or cnt == len(points) - 1:
+            break
+        tpData += ','
+
+    # print tpData
+    tpData += "]\",\"useZip\":false}\n"
+
+    outdata.writelines(tpData)
+
+
+    print tpData
 
 
 def getRoomIdJson():
@@ -100,6 +134,9 @@ def getRoomIdJson():
         # output.writelines(NewJson2)
         # print (str(cnt * 2) + "% :" + ("#" * cnt))
         # print NewJson2
+
+        willSelectedPoint = set()
+
         run_data_str = re.findall(r'{\"allLocJson\":\"\[\{\\"av.+\"useZip\":false}', str(NewJson2))
         if (len(run_data_str) > 0):
             # print cnt + 1
@@ -116,13 +153,23 @@ def getRoomIdJson():
                 # print point_json_str
                 point_json = json.loads(point_json_str)
                 # print point_json
-                isSelectedPoint(float(point_json['lat']), float(point_json['lng']))
+
+                thisPoint = Point(float(point_json['lat']), float(point_json['lng']))
+
+                isPass, addPoint = isSelectedPoint(float(point_json['lat']), float(point_json['lng']))
+
+                if isPass  == 1:
+                    willSelectedPoint.add(addPoint)
+
+            # for p in willSelectedPoint:
+            #     print p
+            createFivePointsStr(willSelectedPoint)
 
             if (cnt >= 5):
                 break
-
-
     output.close()
+    outdata.close()
+
 
 def main():
     getRoomIdJson()
